c726f9738cbe5b0161f4718288fee4baaa78559b
ryman69 <ryan.sansom@uk.tesco.com>
2015-07-30 22:36:06 +0100

    neatened up and added functions
    now won’t accept invalid grid placement

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -8,62 +8,45 @@ function generateTile() {
       console.log(resp.image_url);
       document.getElementById("current-tile2").src = resp.image_url;
       document.getElementById("place-button").disabled = false;
+      document.getElementById("get-tile").disabled = true;
 }
       function rotateLeft() {
             if (document.getElementById("current-tile2").style.transform == "") {
-                  document.getElementById("current-tile2").style.WebkitTransform = "rotate(270deg)";
-                  document.getElementById("current-tile2").style.msTransform = "rotate(270deg)";
-                  document.getElementById("current-tile2").style.transform = "rotate(270deg)";
+                  rotate("current-tile2",270);
             } else if (document.getElementById("current-tile2").style.transform === "rotate(0deg)") {
-                  document.getElementById("current-tile2").style.WebkitTransform = "rotate(270deg)";
-                  document.getElementById("current-tile2").style.msTransform = "rotate(270deg)";
-                  document.getElementById("current-tile2").style.transform = "rotate(270deg)";
+                  rotate("current-tile2",270);
             } else if (document.getElementById("current-tile2").style.transform === "rotate(270deg)") {
-                  document.getElementById("current-tile2").style.WebkitTransform = "rotate(180deg)";
-                  document.getElementById("current-tile2").style.msTransform = "rotate(180deg)";
-                  document.getElementById("current-tile2").style.transform = "rotate(180deg)";
+                  rotate("current-tile2",180);
             } else if (document.getElementById("current-tile2").style.transform === "rotate(180deg)") {
-                  document.getElementById("current-tile2").style.WebkitTransform = "rotate(90deg)";
-                  document.getElementById("current-tile2").style.msTransform = "rotate(90deg)";
-                  document.getElementById("current-tile2").style.transform = "rotate(90deg)";
+                  rotate("current-tile2",90);
             } else if (document.getElementById("current-tile2").style.transform === "rotate(90deg)") {
-                  document.getElementById("current-tile2").style.WebkitTransform = "rotate(0deg)";
-                  document.getElementById("current-tile2").style.msTransform = "rotate(0deg)";
-                  document.getElementById("current-tile2").style.transform = "rotate(0deg)";
+                  rotate("current-tile2",0);
             } else {
                   console.log("unrotatable");
             }
       }
 
       function rotateRight() {
-            console.log(document.getElementById("current-tile2").style.transform);
             if (document.getElementById("current-tile2").style.transform == "") {
-                  document.getElementById("current-tile2").style.WebkitTransform = "rotate(90deg)";
-                  document.getElementById("current-tile2").style.msTransform = "rotate(90deg)";
-                  document.getElementById("current-tile2").style.transform = "rotate(90deg)";
+                  rotate("current-tile2",90);
             } else if (document.getElementById("current-tile2").style.transform === "rotate(0deg)") {
-                  document.getElementById("current-tile2").style.WebkitTransform = "rotate(90deg)";
-                  document.getElementById("current-tile2").style.msTransform = "rotate(90deg)";
-                  document.getElementById("current-tile2").style.transform = "rotate(90deg)";
+                  rotate("current-tile2",90);
             } else if (document.getElementById("current-tile2").style.transform === "rotate(270deg)") {
-                  document.getElementById("current-tile2").style.WebkitTransform = "rotate(0deg)";
-                  document.getElementById("current-tile2").style.msTransform = "rotate(0deg)";
-                  document.getElementById("current-tile2").style.transform = "rotate(0deg)";
+                  rotate("current-tile2",0);
             } else if (document.getElementById("current-tile2").style.transform === "rotate(180deg)") {
-                  document.getElementById("current-tile2").style.WebkitTransform = "rotate(270deg)";
-                  document.getElementById("current-tile2").style.msTransform = "rotate(270deg)";
-                  document.getElementById("current-tile2").style.transform = "rotate(270deg)";
+                  rotate("current-tile2",270);
             } else if (document.getElementById("current-tile2").style.transform === "rotate(90deg)") {
-                  document.getElementById("current-tile2").style.WebkitTransform = "rotate(180deg)";
-                  document.getElementById("current-tile2").style.msTransform = "rotate(180deg)";
-                  document.getElementById("current-tile2").style.transform = "rotate(180deg)";
+                  rotate("current-tile2",180);
             } else {
                   console.log("unrotatable");
             }
       }
       function selectCell(td) {
             if (document.getElementsByClassName(td.className)[0].innerHTML !== "") {
-
+                  if (tile_selected) {
+                        document.getElementsByClassName(tile_selected)[0].style.backgroundColor = "";
+                        row = undefined; column = undefined; tile_selected = undefined;
+                  }
             } else {
             tile_selected = td.className;
             row= Number(tile_selected.substring(4,7));
@@ -90,28 +73,36 @@ function generateTile() {
                   alert("Please select a tile first");
             } else {
                   if (document.getElementsByClassName("tile003003")[0].innerHTML === "" && tile_selected !== "tile003003") {
-                        document.getElementsByClassName("tile003003")[0].innerHTML = "<img id=\""+tile_selected+"\" src=\""+document.getElementById("current-tile2").src+"\">";
-                        document.getElementById(tile_selected).style.WebkitTransform = temp1;
-                        document.getElementById(tile_selected).style.msTransform = temp2;
-                        document.getElementById(tile_selected).style.transform = temp3;
-                        var x = document.getElementsByTagName("td");
-                        for (var i=0;i<x.length;i++) {
-                              x[i].style.backgroundColor = "";
-                              //x[i].innerHTML = "";
-                        }
+                        document.getElementsByClassName("tile003003")[0].innerHTML = "<img id=\"tile003003\" src=\""+document.getElementById("current-tile2").src+"\">";
+                        document.getElementById("tile003003").style.WebkitTransform = temp1;
+                        document.getElementById("tile003003").style.msTransform = temp2;
+                        document.getElementById("tile003003").style.transform = temp3;
+                        document.getElementsByClassName(tile_selected)[0].style.backgroundColor = "";
+                        row = undefined; column = undefined; tile_selected = undefined;
+                        document.getElementById("current-tile2").src = "http://placekitten.com/110/100/";
+                        rotate("current-tile2",0);
+                        document.getElementById("place-button").disabled = true;
+                        document.getElementById("get-tile").disabled = false;
                         alert("First tile, placed in centre");
                   } else {
-                        document.getElementsByClassName(tile_selected)[0].innerHTML = "<img id=\""+tile_selected+"\" src=\""+document.getElementById("current-tile2").src+"\">";
-                        document.getElementById(tile_selected).style.WebkitTransform = temp1;
-                        document.getElementById(tile_selected).style.msTransform = temp2;
-                        document.getElementById(tile_selected).style.transform = temp3;
+                        if (tile_selected === "tile003003" || checkAdjacent()) {
+                              document.getElementsByClassName(tile_selected)[0].innerHTML = "<img id=\""+tile_selected+"\" src=\""+document.getElementById("current-tile2").src+"\">";
+                              document.getElementById(tile_selected).style.WebkitTransform = temp1;
+                              document.getElementById(tile_selected).style.msTransform = temp2;
+                              document.getElementById(tile_selected).style.transform = temp3;
+                              console.log(checkAdjacent());
+                              row = undefined; column = undefined; tile_selected = undefined;
+                              document.getElementById("current-tile2").src = "http://placekitten.com/110/100/";
+                              rotate("current-tile2",0);
+                              document.getElementById("place-button").disabled = true;
+                              document.getElementById("get-tile").disabled = false;
+                        } else {
+                              console.log(checkAdjacent());
+                              alert("Invalid tile placement. Please try again");
+                              document.getElementsByClassName(tile_selected)[0].style.backgroundColor = "";
+                              row = undefined; column = undefined; tile_selected = undefined;
+                        }
                   }
-                  document.getElementById("current-tile2").src = "http://placekitten.com/110/100/";
-                  document.getElementById("current-tile2").style.WebkitTransform = "rotate(0deg)";
-                  document.getElementById("current-tile2").style.msTransform = "rotate(0deg)";
-                  document.getElementById("current-tile2").style.transform = "rotate(0deg)";
-                  document.getElementById("place-button").disabled = true;
-
             }
       }
 function resetTiles() {
@@ -119,4 +110,48 @@ function resetTiles() {
       xmlHttp.open( "GET", '/reset', false );
       xmlHttp.send( null );
       console.log(xmlHttp.responseText);
+      document.getElementById("current-tile2").src = "http://placekitten.com/110/100/";
+      document.getElementById("place-button").disabled = true;
+      document.getElementById("get-tile").disabled = false;
+}
+function rotate(id,deg) {
+      document.getElementById(id).style.WebkitTransform = "rotate("+deg+"deg)";
+      document.getElementById(id).style.msTransform = "rotate("+deg+"deg)";
+      document.getElementById(id).style.transform = "rotate("+deg+"deg)";
+}
+function checkAdjacent() {
+      var rowCheck = row - 1;
+      var colCheck = column;
+      var check = "tile"+convNum(rowCheck)+convNum(colCheck);
+      if (document.getElementById(check)) {
+            return true;
+      }
+      rowCheck = row;
+      colCheck = column-1;
+      check = "tile"+convNum(rowCheck)+convNum(colCheck);
+      if (document.getElementById(check)) {
+            return true;
+      }
+      rowCheck = row;
+      colCheck = column+1;
+      check = "tile"+convNum(rowCheck)+convNum(colCheck);
+      if (document.getElementById(check)) {
+            return true;
+      }
+      rowCheck = row + 1;
+      colCheck = column;
+      check = "tile"+convNum(rowCheck)+convNum(colCheck);
+      if (document.getElementById(check)) {
+            return true;
+      }
+      return false;
+}
+function convNum(num) {
+      if (num<10) {
+            return "00"+num;
+      } else if (num<100) {
+            return "0"+num;
+      } else {
+            return num.toString();
+      }
 }
