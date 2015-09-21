173cba5de4c46cb56f7be8fe08ff3827ee648d33
ryman69 <ryan.sansom@uk.tesco.com>
2015-09-21 17:51:10 +0100

    added CORS headers

--- app.js
+++ app.js
@@ -24,6 +24,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
 app.use('/', routes);
 app.use('/test', testing);
 app.use('/v2', v2);
